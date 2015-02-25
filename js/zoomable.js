47c48482af48080f5b43bfbd980b1f916355ea88
dreamonus <dmarkin@ua.fm>
2015-02-25 20:22:25 +0200

    Added comments; change styles

--- js/zoomable.js
+++ js/zoomable.js
@@ -4,10 +4,12 @@ function makeZoomable(gallery) {
     var ESC_KEYCODE = 27;
     $('#modalWindow').hide();
 
-    $('.' + gallery.classList + ' img').on('click', function (event) {
+    //make preparation for opening modal window with zoomable image
+    $('.' + gallery.classList + ' img').on('click', function () {
         var src = $(this).attr('src').replace('small', 'large');
         openModalImage(src);
 
+        //if window resized make resizing of the image
         $(window).on('resize', function () {
             $('#modalWindow > div').css({
                 left: '',
@@ -18,6 +20,7 @@ function makeZoomable(gallery) {
             computeImageSizes();
         });
 
+        //close modal window on ESC
         $(window).on('keyup', function (event) {
             if (event.keyCode === ESC_KEYCODE) {
                 closeModalWindow();
@@ -25,11 +28,13 @@ function makeZoomable(gallery) {
         })
     });
 
+    //open modal window with zoomable image
     function openModalImage(src) {
         if ($('#modalWindow img').length === 0) {
             $('#modalWindow').append($('<div>').append($('<img>').attr('src', src)).append($('<div>').addClass('close-button').html('&times;')));
         }
         else {
+            //if img-nod� was created - change source link on image inside it and clear position parameters
             $('#modalWindow img').attr('src', src);
             $('#modalWindow > div').css({
                 left: '',
@@ -39,36 +44,49 @@ function makeZoomable(gallery) {
             });
         }
         $('#modalWindow').show();
+
+        //recalculate images sizes and position
         $('#modalWindow img').off('load');
         $('#modalWindow img').on('load', function () {
             computeImageSizes();
         });
         $('#bgOverlay').append('<div id="gray-overlay"></div>');
+
+        //set listener for close button
         $('.close-button').on('click', function () {
             $('.close-button').off('click');
             closeModalWindow();
         })
     }
 
+    //calculate image sizes and position parameters
     function computeImageSizes(imageWidth, imageHeight) {
         var imageInWindowWidth;
         var imageInWindowHeight;
         var padding = 0.1;
+
         var imageWidth = $('#modalWindow img').width();
         var imageHeight = $('#modalWindow img').height();
+
         var viewportWidth = $(window).width() * (1 - 2 * padding);
         var viewportHeigth = $(window).height() * (1 - 2 * padding);
+
         var flexWidthCoefficient = imageWidth / viewportWidth;
         var flexHeightCoefficient = imageHeight / viewportHeigth;
         var flexMaxCoefficient = Math.max(flexWidthCoefficient, flexHeightCoefficient);
 
+        //case if large image have been placed fully at viewport
         if (flexMaxCoefficient <= 1) {
             imageInWindowWidth = imageWidth;
             imageInWindowHeight = imageHeight;
         } else {
+
+        //case if large image not have been placed fully at viewport - zooming image
             imageInWindowWidth = imageWidth / flexMaxCoefficient;
             imageInWindowHeight = imageHeight / flexMaxCoefficient;
         }
+
+        //centering large image at viewport
         var left = ($(window).width() - imageInWindowWidth) / 2;
         var top = ($(window).height() - imageInWindowHeight) / 2;
 
--- readme.md
+++ readme.md
@@ -1,12 +1,13 @@
-**This function for zooming images**:
+﻿**This function for zooming images**:
 * when clicking on the image shows an enlarged view, obscuring the entire page (without possibility to click on contents on page or to select the text with the mouse);
 * larger image always centered vertically and horizontally of the screen (even if the size of the document is changed);
 * link to large image different from link of preview image only a folder name (for example: img/small/dog.jpg and img/large/dog.jpg);
 * larger image hides when user clicks on the close block or presses Esc;
 * close button is always in the top right corner of the enlarged image;
 * when scrolling the contents of the page scrolls, and an enlarged image is centered.
+Used jQuery.
+*Function works in IE9 +.*
 
-*Used jQuery, function works in IE9 +.*
 
 **Функция масштабирования изображений**:
 * при клике по изображению показывалось увеличенное изображение, затемняя всю страницу (без возможности кликнуть по содержимому страницы, выделить текст мышкой);
@@ -15,5 +16,5 @@
 * увеличенное изображение скрывается при клике на крестике или при нажатии Esc;
 * кнопка закрытия всегда находится в правом верхнем углу увеличенного изображения;
 * при скроллинге содержимое страницы скроллится, а увеличенное изображение остается по центру.
-
-*Использована jQuery, функция работает в IE9+.*
+Использована jQuery.
+*Функция работает в IE9+.*
